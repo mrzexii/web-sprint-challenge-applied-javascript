dc96a4f7035ca86803402547daa4aa02b1010ec2
mrzexii <126282962+mrzexii@users.noreply.github.com>
2023-08-31 21:23:23 -0700

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,39 @@
+// Modify the Card component to accept an article object
 const Card = (article) => {
+  // Create the card elements
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+
+  const headlineDiv = document.createElement('div');
+  headlineDiv.classList.add('headline');
+  headlineDiv.textContent = article.headline;
+
+  const authorDiv = document.createElement('div');
+  authorDiv.classList.add('author');
+
+  const divImgContainer = document.createElement('div');
+  divImgContainer.classList.add('img-container');
+
+  // Create the image element
+  const img = document.createElement('img');
+  img.src = article.authorPhoto; // Set the image source
+
+  const span = document.createElement('span');
+  span.textContent = `By ${article.authorName}`;
+
+  // Append the elements to the card
+  authorDiv.appendChild(divImgContainer);
+  divImgContainer.appendChild(img);
+  authorDiv.appendChild(span);
+
+  cardDiv.appendChild(headlineDiv);
+  cardDiv.appendChild(authorDiv);
+
+  return cardDiv;
+};
+
+
+
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -17,9 +52,37 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-}
 
-const cardAppender = (selector) => {
+
+  const cardAppender = (selector) => {
+    fetch(`http://localhost:5001/api/articles`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.articles) {
+          const targetElement = document.querySelector(selector);
+          if (targetElement) {
+            // Iterate through topics and articles to create cards
+            Object.values(data.articles).forEach((articlesArray) => {
+              articlesArray.forEach((article) => {
+                const card = Card(article);
+                targetElement.appendChild(card);
+              });
+            });
+          } else {
+            console.error('element not found');
+          }
+        } else {
+          console.error('invalid response');
+        }
+      })
+      .catch((error) => {
+        console.error('fetch error', error);
+      });
+  };
+  
+
+  
+
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
@@ -28,6 +91,6 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
-}
+
 
 export { Card, cardAppender }
